b7be105d014d534a19b19e9a061a4a2ec715f670
GabrielMS02 <148493245+GabrielMS02@users.noreply.github.com>
2024-02-29 20:21:09 +0000

    29/02/2024

---
+++ Semana03 - Aula04/Questão 3 - Manipulação/Q17.py
@@ -0,0 +1,5 @@
+#Escreva um programa que solicite ao usuário para digitar uma frase e conte quantas palavras existem na frase.
+
+frase = input('Digite uma frase: ')
+separar = len(frase.split())
+print(separar)
